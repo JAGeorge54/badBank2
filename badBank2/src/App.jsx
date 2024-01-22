9450e75e89bf1b8ff8a91253f949dff44d379e6b
JAGeorge54 <137643997+JAGeorge54@users.noreply.github.com>
2024-01-22 11:44:39 -0500

    added routes

--- badBank2/src/App.jsx
+++ badBank2/src/App.jsx
@@ -1,6 +1,12 @@
 import { useState } from 'react'
 import { Route, Routes } from 'react-router-dom'
 import { Home } from './pages/Home'
+import { Login } from './pages/Login'
+import { Createuser } from './pages/Createuser'
+import { Deposit } from './pages/Deposit'
+import { Withdrawl } from './pages/Withdrawl'
+import { Balance } from './pages/Balance'
+import { Alldata } from './pages/Alldata'
 import './App.css'
 
 function App() {
@@ -9,7 +15,12 @@ function App() {
   return (
     <Routes>
       <Route path='/' element={<Home />} />
-      <Route />
+      <Route path='/login' element={ <Login/> } />
+      <Route path='/createuser' element={ <Createuser/> } />
+      <Route path='/deposit' element={ <Deposit/> } />
+      <Route path='/withdrawl' element={ <Withdrawl/> } />
+      <Route path='/balance' element={ <Balance/> } />
+      <Route path='/alldata' element={ <Alldata/> } />
     </Routes>
   )
 }
