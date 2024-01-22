532026015c827185cf3088b600c3ee32ee6ea17d
JAGeorge54 <137643997+JAGeorge54@users.noreply.github.com>
2024-01-22 18:16:19 -0500

    setting up context

--- badBank2/src/App.jsx
+++ badBank2/src/App.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react'
+import React, { useState } from 'react'
 import { Route, Routes } from 'react-router-dom'
 import { NavBar } from './componenets/Navbar'
 import { Home } from './pages/Home'
@@ -8,24 +8,31 @@ import { Deposit } from './pages/Deposit'
 import { Withdrawl } from './pages/Withdrawl'
 import { Balance } from './pages/Balance'
 import { Alldata } from './pages/Alldata'
-import './App.css'
 import 'bootstrap/dist/css/bootstrap.min.css';
+export const AllDataContext = React.createContext()
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [data, setData] = useState([{
+    name: 'able',
+    email: 'able@mit.edu',
+    password: 'secret',
+    balance: 100
+  }])
 
   return (
     <>
       <NavBar />
-      <Routes>
-        <Route path='/' element={<Home />} />
-        <Route path='/login' element={ <Login/> } />
-        <Route path='/createuser' element={ <Createuser/> } />
-        <Route path='/deposit' element={ <Deposit/> } />
-        <Route path='/withdrawl' element={ <Withdrawl/> } />
-        <Route path='/balance' element={ <Balance/> } />
-        <Route path='/alldata' element={ <Alldata/> } />
-      </Routes>
+      <AllDataContext.Provider value={data}>
+        <Routes>
+          <Route path='/' element={<Home />} />
+          <Route path='/login' element={ <Login/> } />
+          <Route path='/createuser' element={ <Createuser/> } />
+          <Route path='/deposit' element={ <Deposit/> } />
+          <Route path='/withdrawl' element={ <Withdrawl/> } />
+          <Route path='/balance' element={ <Balance/> } />
+          <Route path='/alldata' element={ <Alldata/> } />
+        </Routes>
+      </AllDataContext.Provider>
     </>
   )
 }
