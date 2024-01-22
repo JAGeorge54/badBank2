36e404053a3c5df89e4d61297f4e0034088a8be6
JAGeorge54 <137643997+JAGeorge54@users.noreply.github.com>
2024-01-22 14:56:16 -0500

    routed links in navbar

--- badBank2/src/componenets/Navbar.jsx
+++ badBank2/src/componenets/Navbar.jsx
@@ -9,26 +9,22 @@ export function NavBar() {
     <>
       <Navbar collapseOnSelect expand="lg" className="bg-body-tertiary">
         <Container>
-          <Navbar.Brand href="#home">Bad Bank</Navbar.Brand>
+          <Navbar.Brand href="/">Bad Bank</Navbar.Brand>
           <Navbar.Toggle aria-controls="responsive-navbar-nav" />
           <Navbar.Collapse id="responsive-navbar-nav">
             <Nav className="me-auto">
-              <Nav.Link href="#features">Home</Nav.Link>
+              <Nav.Link href="/">Home</Nav.Link>
               <NavDropdown title="Dropdown" id="collapsible-nav-dropdown">
-                <NavDropdown.Item href="#action/3.1">Balance</NavDropdown.Item>
-                <NavDropdown.Item href="#action/3.2">Deposit</NavDropdown.Item>
-                <NavDropdown.Item href="#action/3.3">Withdrawl</NavDropdown.Item>
+                <NavDropdown.Item href="/balance">Balance</NavDropdown.Item>
+                <NavDropdown.Item href="/deposit">Deposit</NavDropdown.Item>
+                <NavDropdown.Item href="/withdrawl">Withdrawl</NavDropdown.Item>
                 <NavDropdown.Divider />
-                <NavDropdown.Item href="#action/3.4">
-                  Alldata
-                </NavDropdown.Item>
+                <NavDropdown.Item href="/alldata">Alldata</NavDropdown.Item>
               </NavDropdown>
             </Nav>
             <Nav>
-              <Nav.Link href="#deets">Create User</Nav.Link>
-              <Nav.Link eventKey={2} href="#memes">
-                Login
-              </Nav.Link>
+              <Nav.Link href="/createuser">Create User</Nav.Link>
+              <Nav.Link eventKey={2} href="/login">Login</Nav.Link>
             </Nav>
           </Navbar.Collapse>
         </Container>
