00ee6711fc0149168c09963e01135196e564dbe6
JAGeorge54 <137643997+JAGeorge54@users.noreply.github.com>
2024-01-22 18:16:35 -0500

    setting up alldata card

--- badBank2/src/pages/Alldata.jsx
+++ badBank2/src/pages/Alldata.jsx
@@ -1,3 +1,41 @@
+import React, { useState, useContext } from 'react'
+import { AllDataContext } from '../App'
+import Card from 'react-bootstrap/Card';
+
 export function Alldata() {
-  return <h1>Alldata</h1>
+  const users = useContext(AllDataContext);
+  const balance = users.balance;
+
+  const User = () => {
+    const newUser = users.map((user, i) => {
+      const name = user.name
+      const email = user.email
+      const password = user.password
+      const balance = user.balance
+      return (
+        <Card
+          bg='light'
+          key='light'
+          text='dark'
+          style={{ width: '18rem' }}
+          className="mb-2"
+        >
+          <Card.Header>{name}</Card.Header>
+          <Card.Body>
+            <Card.Title>{email}</Card.Title>
+            <Card.Title>Password: {password}</Card.Title>
+            <Card.Title>Balance: ${balance}</Card.Title>
+          </Card.Body>
+        </Card>
+      )
+    })
+
+    return newUser;
+  }
+
+  return (
+    <>
+      <User />
+    </>
+  )
 }
